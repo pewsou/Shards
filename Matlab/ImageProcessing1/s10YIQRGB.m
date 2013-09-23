cbd265660f092a7691ee55c3904245ac34454e1a
pewsou <bvprojs@gmail.com>
2013-09-23 11:19:55 +0300

    another exercise of color conversion and histogram

---
+++ Matlab/ImageProcessing1/s10YIQRGB.m
@@ -0,0 +1,41 @@
+close all
+clear all
+
+In=imread('anna500x332.tif');
+
+%RGB = [128 128 128]'
+YIQ2RGB=[[0.299 0.596 0.212];[0.587 -0.275 -0.523];[0.114 -0.321 0.311];]';
+
+T=double(In)/255;
+
+Out=zeros(size(T));
+Res=zeros(size(T));
+
+lr=length(T(:,1,1));
+lc=length(T(1,:,1));
+
+for j=1:lc
+    for i=1:lr
+        S=(YIQ2RGB * [T(i,j,1);T(i,j,2); T(i,j,3);]);
+        Out(i,j,1)=S(1);
+        Out(i,j,2)=S(2);
+        Out(i,j,3)=S(3);
+    end
+end
+
+Out(:,:,1)=histeq(Out(:,:,1));
+
+for j=1:lc
+    for i=1:lr
+        S=inv(YIQ2RGB)*[Out(i,j,1);Out(i,j,2); Out(i,j,3);];
+        Res(i,j,1)=S(1);
+        Res(i,j,2)=S(2);
+        Res(i,j,3)=S(3);
+    end
+end
+
+figure;imshow(In);
+figure;imshow(Res);
+
+%figure;imhist(hgram);
+%figure;imhist(T(:,:,1));
